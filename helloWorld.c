1c02fb092e0dc08021be889e53b8ce709aab4f01
ForelBezo <bewebbez@gmail.com>
2023-12-25 00:29:46 +0300

    helloWorld

---
+++ helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+    int numberOfCars;
+    numberOfCars = 3;
+
+    printf("There are %d cars\n", numberOfCars);
+
+    return 0;
+}
