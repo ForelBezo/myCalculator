90098d977d0b79d5284ac4c264ca5528b2f7f3d9
ForelBezo <bewebbez@gmail.com>
2024-01-02 21:08:23 +0300

    feat:Ty to use for

---
+++ drinvingRule.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to the exam of therical session of chess.\nIt's a game be fun and enjoy!\n");
+    
+    char question1[10];
+    int question2;
+    char question3[10];
+
+    for (attempt = ){
+
+    }
+
+    
+    return 0;   
+}
